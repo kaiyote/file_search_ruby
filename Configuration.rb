3220876d10bd7b6d59bf0f2e69f10e11aa7196b9
Tim Huddle <blazhen88@gmail.com>
2013-08-25 17:07:33 -0400

    added attributes and load/save functionality to configuration class

--- Configuration.rb
+++ Configuration.rb
@@ -1,9 +1,28 @@
-class Configuration
-  @@_directory = "~\AppData\Roaming\PhraseFinder\"
-  @@_filename = @@_directory + "settings.dat"
+require 'fileutils'
 
-  @_rootDirectory
-  @_notepadPath
-  @_excludedFolders
-  @_extensions
+class Configuration
+	@@_directory = '~\\AppData\\Roaming\\PhraseFinder\\'
+	@@_filename = @@_directory + 'settings.dat'
+
+	attr_accessor :rootDirectory
+	attr_accessor :editorPath
+	attr_accessor :excludedFolders
+	attr_accessor :extensions
+
+	def Save
+		unless File.directory?(@@_directory)
+			FileUtils.mkdir_p(@@_directory)
+		end
+		File.open(@@_directory + @@_filename, 'w') do |file|
+			file.print Marshal.dump(self)
+		end
+	end
+  
+	def self.Load
+		if File.exists?(@@_directory + @@_filename)
+			File.open(@@_directory + @@_filename, 'r') do |file|
+				Marshal.load(file)
+			end
+		end
+	end
 end
